bba037cc58c7b366a39762715af0523736de88e6
imjchiang <jchiang296@gmail.com>
2020-08-04 23:36:03 -0700

    [JS] add about and blog pages

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) =>
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/about", (req, res) =>
+{
+    res.sendFile(__dirname + "/views/about.html");
+})
+
+app.get("/blog", (req, res) =>
+{
+    res.sendFile(__dirname + "/views/blog-directory.html");
+})
+
 app.listen(8000, () =>
 {
     console.log("Listening to port 8000");
