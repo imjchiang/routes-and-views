5b51572c376ffdbf9ae3ad807f7550352e33e0ac
imjchiang <jchiang296@gmail.com>
2020-08-05 13:59:41 -0700

    [NPM] add ejs package [JS] set ejs view engine

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+//home route
 app.get("/", (req, res) =>
 {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+//about route
 app.get("/about", (req, res) =>
 {
     res.sendFile(__dirname + "/views/about.html");
 })
 
+//blog route
 app.get("/blog", (req, res) =>
 {
     res.sendFile(__dirname + "/views/blog-directory.html");
 })
 
+//listen to port 8000
 app.listen(8000, () =>
 {
     console.log("Listening to port 8000");
