ae20dd7ebe774727cebf1fde0b5e0318bea85996
imjchiang <jchiang296@gmail.com>
2020-08-05 14:07:29 -0700

    [JS] implement ejs [EJS] convert html to ejs

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const express = require("express");
 
 const app = express();
 
+//tell express we are going to use ejs as the view engine
 app.set("view engine", "ejs");
 
 //home route
 app.get("/", (req, res) =>
 {
-    res.sendFile(__dirname + "/views/index.html");
+    //express will look inside views folder by default for an ejs file with this name
+    res.render("index");
+    //res.sendFile(__dirname + "/views/index.html");
 });
 
 //about route
 app.get("/about", (req, res) =>
 {
-    res.sendFile(__dirname + "/views/about.html");
+    res.render("about");
+    //res.sendFile(__dirname + "/views/about.html");
 })
 
 //blog route
 app.get("/blog", (req, res) =>
 {
-    res.sendFile(__dirname + "/views/blog-directory.html");
+    res.render("blog-directory");
+    //res.sendFile(__dirname + "/views/blog-directory.html");
 })
 
 //listen to port 8000
